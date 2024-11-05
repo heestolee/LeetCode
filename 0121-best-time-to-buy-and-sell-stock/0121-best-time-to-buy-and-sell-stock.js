f2eb09e6e43935274953913dd45f45b4bccac5bf
heestolee <160560292+heestolee@users.noreply.github.com>
2024-11-05 09:10:00 +0900

    Time: 2 ms (75.39%), Space: 59.3 MB (34.01%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+        
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+    }
+    
+    return maxProfit;
+};
